4a403544925bf5fc68fd782351b792bad3ab43a7
dennisbalan <dbalan2234@gmail.com>
2020-10-27 09:15:17 -0700

    getAnswer.c is ddone

--- getAnswer.c
+++ getAnswer.c
@@ -1,5 +1,24 @@
 char getAnswer(){
-
-
-
-
+	printf("Was my number too high, too low, or correct?\n");
+	char a[120];
+	char b;
+	int c,d;
+	d = 0
+	while(d == 0){
+		fgets(a,120, stdin);
+		c = sscanf(a,"%c",&b);
+		if(c == 1){
+			if((b == 'c') || (b == 'h') || (b == 'l')){
+				return b;
+			else{
+				printf("wrong number");
+				d = 0;
+			}				
+		}
+		else{
+			printf("too much stuff written in");
+			d = 0;
+		}	
+		}	
+	}	
+}

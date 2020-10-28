48c6a8a9ed3a1cb42b8cec919b134b71ffa6e55b
dennisbalan <dbalan2234@gmail.com>
2020-10-28 09:47:54 -0700

    prediction function correct

--- main.c
+++ main.c
@@ -4,29 +4,53 @@
 #include "main.h"
 
 int main(int argc, char ** argv){
-	intro();
 	int a,b;
 	if(argc == 1){
 		a = 0;
 		b = 100;
+		intro();
 		show_num_range(a,b);
 	}
 	else if(argc == 2){
 		a = 0;
-		sscanf(argv[1],"%d",&b);
-		show_num_range(a,b);
+		char z[120];
+		int w;
+		w = sscanf(argv[1],"%d%s",&b,z);
+		if(w == 1){
+			if( a >= b){
+				printf("inavlid assignment\n");
+				return 0;
+			}
+			intro();
+			show_num_range(a,b);
+		}
+		else{
+			printf("You number is not good enough\n");
+			return 0;
+		}
 	}
 	else if(argc == 3){
-		sscanf(argv[1],"%d",&a);
-		sscanf(argv[2],"%d",&b);
-		show_num_range(a,b);
+		char x[120];
+		char y[120];
+		int l, v;
+		l = sscanf(argv[1],"%d%s",&a,x);
+		v = sscanf(argv[2],"%d%s",&b,y);
+		if((l == 1) && (v == 1)){
+			if(a >= b){
+				printf("Invalid assignment\n");
+				return 0;
+			}
+			intro();
+			show_num_range(a,b);
+		}
+		
+		else{
+			printf("You numbers are not good enough\n");
+			return 0;
+		}
 	}
 	else{
 		printf("too many arguements\n");
-		return 0;
-	}
-	if(a == b){
-		printf("you can not have the same numbers\n");
 		return 0;
 	}
 	int c;
--- predict.c
+++ predict.c
@@ -1,10 +1,12 @@
 #include <math.h>
 int predict(int low, int high){
-	double a,b,f;
+	double a,b,f,h,j;
 	int c;
-	a = (double) high - low;
-	b = log(1+a);
-	f = 1 +b;
+	a = (double) high;
+	h = (double) low;
+	j = a - h;
+	b = log2(1+j);
+	f = 1 + b;
 	c = (int) f;
 	return c;
 }
